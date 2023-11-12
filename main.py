880490effd9b66071040cdd10bc223c3f1f3fbc8
vinzenz-fischer <vhfischer@outlook.de>
2023-11-12 15:30:48 +0100

    separated pressing and releasing of mouse buttons

--- main.py
+++ main.py
@@ -21,15 +21,20 @@ def on_press(key: KeyCode|Key):
     if Keybinds.MOVE_LEFT    == key: pag.move(-speed, 0)
     if Keybinds.MOVE_DOWN    == key: pag.move(0,  speed)
     if Keybinds.MOVE_RIGHT   == key: pag.move( speed, 0)
-    if Keybinds.CLICK_LEFT   == key: pag.click(button=pag.LEFT)
-    if Keybinds.CLICK_RIGHT  == key: pag.click(button=pag.RIGHT)
-    if Keybinds.CLICK_MIDDLE == key: pag.click(button=pag.MIDDLE)
+    if Keybinds.CLICK_LEFT   == key: pag.mouseDown(button=pag.LEFT)
+    if Keybinds.CLICK_RIGHT  == key: pag.mouseDown(button=pag.RIGHT)
+    if Keybinds.CLICK_MIDDLE == key: pag.mouseDown(button=pag.MIDDLE)
     if Keybinds.SCROLL_UP    == key: pag.scroll(10)
     if Keybinds.SCROLL_DOWN  == key: pag.scroll(-10)
 
+def on_release(key: KeyCode|Key):
+    if Keybinds.CLICK_LEFT   == key: pag.mouseUp(button=pag.LEFT)
+    if Keybinds.CLICK_RIGHT  == key: pag.mouseUp(button=pag.RIGHT)
+    if Keybinds.CLICK_MIDDLE == key: pag.mouseUp(button=pag.MIDDLE)
+
 def main():
     global listener
-    listener = Listener(on_press=on_press)
+    listener = Listener(on_press=on_press, on_release=on_release)
     with listener:
         listener.join()
 
